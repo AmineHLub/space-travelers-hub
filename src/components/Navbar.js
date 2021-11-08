32324e7ae469e5d8a0dcd5e7141be7faad2359be
AmineHLub <Amino.bhl@gmail.com>
2021-11-08 13:01:05 +0100

    ReAdjust Navbar
    Add elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { NavLink } from 'react-router-dom';
 import mylogo from './assets/images/mylogo.png';
 import './assets/Navbar.css';
 // create navbar
 const Navbar = () => (
   <nav className="navbar">
-    <div className="logoContainer">
-      <img className="logo" alt="" src={mylogo} />
+    <div className="navbar-container">
+      <div className="logoContainer">
+        <img className="logo" alt="" src={mylogo} />
+        <h1> Space Traveler&apos;s hub </h1>
+      </div>
+      <div className="linksContainer">
+        <ul className="nav-elements">
+          <li>Rocket</li>
+          <li>Missions</li>
+          <li>My Profile</li>
+        </ul>
+      </div>
     </div>
-    <div className="linksContainer" />
   </nav>
 );
 
