16bea950a44f1e716034806edceb238710d199ea
AmineHLub <Amino.bhl@gmail.com>
2021-11-08 18:48:13 +0100

    Add adjustments to rocket componenent's layout
    Add border bottom to navbarand minor other fixes

--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,29 +1,26 @@
 import React from 'react';
+import '../assets/styles/rocketspage.css';
 
 const Rockets = (rocketsObject) => {
   const { rockets } = rocketsObject;
   return (
+    <div className="rockets-container d-flex">
+      {
     rockets.map((el) => (
-      <div key={el.id}>
-        <div>
-          id :
-          {' '}
-          {el.id}
+      <div className="rocket-container d-flex" key={el.id}>
+        <div className="img-container">
+          <img src={el.flickr_images} alt="rocket-img" />
         </div>
-        <div>
-          name :
-          {' '}
-          {el.rocket_name}
+        <div className="info-container d-flex">
+          <span>{el.id}</span>
+          <h2 className="rocket-name">{el.rocket_name}</h2>
+          <h3 className="rocket-type">{el.rocket_type}</h3>
         </div>
-        <div>
-          type :
-          {' '}
-          {el.rocket_type}
-        </div>
-        <span>img: </span>
-        <img src={el.flickr_images} alt="img" />
       </div>
     ))
+  }
+
+    </div>
   );
 };
 export default Rockets;
