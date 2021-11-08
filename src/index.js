c79cfda6452bff6d64819cd76f3201698123bb7f
Lordkaito <cespedesisai88@gmail.com>
2021-11-08 11:27:21 +0100

    Navbar template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
