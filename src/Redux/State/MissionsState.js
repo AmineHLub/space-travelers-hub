a94fd4d68357274f715d98118b276c4abba5fec2
Lordkaito <cespedesisai88@gmail.com>
2021-11-09 21:24:26 +0100

    Fetch data and show the table with the response

--- src/Redux/State/MissionsState.js
+++ src/Redux/State/MissionsState.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const URL = 'https://api.spacexdata.com/v3/missions';
 const GET_MISSIONS = 'spacetraveler/missions/GET_MISSIONS';
 
-const fetchMissions = () => async (dispatch) => {
+export const fetchMissions = () => async (dispatch) => {
   const response = await Axios.get(URL);
   dispatch({
     type: GET_MISSIONS,
--- src/components/pages/Missions.js
+++ src/components/pages/Missions.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import '../assets/styles/missions.css';
 
-const Missions = () => {
+const Missions = (missionsObj) => {
+  const { missions } = missionsObj;
+
   return (
     <>
-      <button
-        type="button"
-      >
-        Fetch Mission
-      </button>
-      <div>
-        empty
+      <h1>Missions</h1>
+      <div className="missions">
+        <table>
+          <thead>
+            <tr>
+              <th className="tableHeader">Mission</th>
+              <th className="tableHeader">Description</th>
+              <th className="tableHeader">Status</th>
+              <th className="tableHeader">This will be empty</th>
+            </tr>
+          </thead>
+          <tbody>
+            {missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>
+                  <h3>
+                    {mission.mission_name}
+                  </h3>
+                </td>
+                <td>{mission.description}</td>
+                <td>
+                  <span
+                    className="notMember"
+                  >
+                    Not a member
+                  </span>
+                </td>
+                <td className="joinButton">
+                  <button
+                    type="button"
+                    className="joinButton"
+                  >
+                    Join
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
