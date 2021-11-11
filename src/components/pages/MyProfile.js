927abfda16c74a5d6dd28083fc9d55024e648847
AmineHLub <Amino.bhl@gmail.com>
2021-11-11 14:18:34 +0100

    Add logic for myProfile page : rockets + missions

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import '../assets/styles/profile.css';
 
-export default function MyProfile() {
+export default function MyProfile(stateObject) {
+  const { missions, rockets } = stateObject;
+  const idsOfRockets = Object.keys(rockets);
+  const resultOfRockets = idsOfRockets.filter((id) => rockets[id].reservation === true);
+  const idsOfmissions = Object.keys(missions);
+  const resultOfmissions = idsOfmissions.filter((id) => missions[id].reservation === true);
   return (
     <div className="profile-container">
       <section className="mymission-items">
         <h2>My Missions</h2>
-        <ul className="items-list">
-          <li>Telstar</li>
-          <li>Telstar</li>
-          <li>Telstar</li>
-          <li>Telstar</li>
-        </ul>
+        {resultOfmissions.length ? (
+          <ul className="items-list">
+            {resultOfmissions.map((joined) => {
+              return (
+                <li key={joined}>{missions[joined].information.mission_name}</li>
+              );
+            })}
+          </ul>
+        ) : null}
       </section>
       <section className="myrocket-items">
         <h2>My Rockets</h2>
-        <ul className="items-list">
-          <li>Telstar</li>
-          <li>Telstar</li>
-          <li>Telstar</li>
-          <li>Telstar</li>
-        </ul>
+        {resultOfRockets.length ? (
+          <ul className="items-list">
+            {resultOfRockets.map((reserved) => {
+              return (
+                <li key={reserved}>{rockets[reserved].information.rocket_name}</li>
+              );
+            })}
+          </ul>
+        ) : null}
       </section>
     </div>
   );
